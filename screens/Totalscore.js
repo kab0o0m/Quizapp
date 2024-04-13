061682461a43a017353cbcab47f3f3a603a21483
kab0o0m <134916365+kab0o0m@users.noreply.github.com>
2024-04-13 13:40:22 +0800

    added retrieve user

--- screens/Totalscore.js
+++ screens/Totalscore.js
@@ -1,14 +1,18 @@
-import React, { useState } from 'react'
-import { View } from 'react-native'
+import React, { useEffect, useState } from "react";
+import { View } from "react-native";
 
-export default function Totalscore () {
-  const [score,setScore] = useState(0)
+export default function Totalscore() {
+  const [score, setScore] = useState(0);
+  const [user, setUser] = useState("");
+
+  useEffect(() => {});
+
+  const retrieveUser = async () => {};
 
   return (
     <View>
       <Text>Total Score</Text>
       <Text>{score}</Text>
     </View>
-  )
+  );
 }
-
